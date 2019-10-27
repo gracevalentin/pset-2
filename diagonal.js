a18000660093e622d64122b719dbbcfd9f5884ef
gracevalentin <55455941+gracevalentin@users.noreply.github.com>
2019-10-27 18:55:42 -0400

    Delete diagonal.js

--- diagonal.js
+++
@@ -1,8 +0,0 @@
-const readlineSync = require ("readline-sync")
-
-let A=readlineSync.question("Width: ")
-let B=readlineSync.question("Length: ")
-let Diagonal= Math.sqrt(A*A + B*B)
-Diagonal=Diagonal.toLocaleString("en", {minimumFractionDigits: 2, maximumFractionDigits: 2})
-
-console.log ("A(n) " + A + "-by-"+ B + "-inch sheet of paper has a diagonal of " + Diagonal + " inch(es).")
